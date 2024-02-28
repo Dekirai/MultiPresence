22e1f4e138df3fd9e3129189b62d00171779d1d7
Dekirai <keivo1997@gmail.com>
2024-02-28 18:45:17 +0100

    Update RE6.cs

--- MultiPresence/Presence/RE6.cs
+++ MultiPresence/Presence/RE6.cs
@@ -33,13 +33,39 @@ namespace MultiPresence.Presence
             if (game.Length > 0)
             {
                 int stage_get = mem.Read2Byte("BH6.exe+1466884,0xA422C");
+                int state_get = mem.Read2Byte("BH6.exe+1466884,0xA4228");
                 var stagevalue = await Stages.GetStage(stage_get);
 
                 string[] stage = stagevalue.Split(':');
 
+                if (state_get == 0)
+                {
+                    discord.UpdateDetails($"Starting the game...");
+                    discord.UpdateState($"");
+                }
+                else if (state_get > 1 && state_get < 9)
+                {
+                    discord.UpdateDetails($"{stage[0]}");
+                    discord.UpdateState($"{stage[1]}");
+                }
+                else if (state_get == 20)
+                {
+                    discord.UpdateDetails($"In Main Menu");
+                    discord.UpdateState($"");
+                }
+                else if (state_get == 10)
+                {
+                    discord.UpdateDetails($"Saving...");
+                    discord.UpdateState($"");
+                }
+                else if (state_get > 20)
+                {
+                    discord.UpdateDetails($"{stage[0]}");
+                    discord.UpdateState($"In a cutscene");
+                }
+
                 discord.UpdateLargeAsset($"logo", $"Resident Evil 6");
-                discord.UpdateDetails($"{stage[0]}");
-                discord.UpdateState($"{stage[1]}");
+
 
                 await Task.Delay(3000);
                 Thread thread = new Thread(RPC);
