4c94eb5882088e199fd5824456919823a9cc6c9f
Dekirai <keivo1997@gmail.com>
2024-03-05 12:03:31 +0100

    [REV2] Changed how Stages work

---
+++ MultiPresence/Models/REV2/Chapters.cs
@@ -0,0 +1,50 @@
+﻿namespace MultiPresence.Models.REV2
+{
+    public class Chapters
+    {
+        public static async Task<string> GetChapter(int chapter)
+        {
+            string getchapter;
+
+            switch (chapter)
+            {
+                case 1:
+                    getchapter = "I";
+                    break;
+                case 2:
+                    getchapter = "II";
+                    break;
+                case 3:
+                    getchapter = "III";
+                    break;
+                case 4:
+                    getchapter = "IV";
+                    break;
+                case 5:
+                    getchapter = "V";
+                    break;
+                case 6:
+                    getchapter = "VI";
+                    break;
+                case 7:
+                    getchapter = "VII";
+                    break;
+                case 8:
+                    getchapter = "VIII";
+                    break;
+                case 9:
+                    getchapter = "?";
+                    break;
+                case 10:
+                    getchapter = "IX";
+                    break;
+
+
+                default:
+                    getchapter = "Unknown";
+                    break;
+            }
+            return getchapter;
+        }
+    }
+}
--- MultiPresence/Presence/REV2.cs
+++ MultiPresence/Presence/REV2.cs
@@ -10,6 +10,7 @@ namespace MultiPresence.Presence
         static Mem mem = new Mem();
         static string process = "rerev2";
         private static DiscordRpcClient discord;
+        static int mission = 0;
         public static async void DoAction()
         {
             GetPID();
@@ -33,19 +34,31 @@ namespace MultiPresence.Presence
             if (game.Length > 0)
             {
                 int raid_character_get = mem.ReadByte("rerev2.exe+117ED54,0x4A58");
+                int raid_chapter_get = mem.ReadByte("rerev2.exe+117ED54,0x30");
                 int raid_character_level = mem.ReadByte("rerev2.exe+117ED54,0x4A59");
                 int raid_money = mem.ReadInt("rerev2.exe+117D120,0xBA08");
                 int stage_get = mem.Read2Byte("rerev2.exe+115AACC");
 
                 var stage = await Stages.GetStage(stage_get);
+                var chapter = await Chapters.GetChapter(raid_chapter_get);
 
                 var raid_character = await Characters.GetCharacter(raid_character_get);
 
                 if (stage[0] == "Raid Mode")
                 {
-                    discord.UpdateLargeAsset($"logo", $"Resident Evil Revelations 2");
-                    discord.UpdateDetails($"Raid Mode: {stage[1]}");
-                    discord.UpdateState($"{raid_character} (Lv. {raid_character_level})");
+                    if (stage[1] == "In Lobby")
+                    {
+                        mission = mem.ReadByte("rerev2.exe+011DE690,0x1E0,0x4C,0x3C,0x14,0x3C,0x74,0x7C4") + 1;
+                        discord.UpdateLargeAsset($"logo", $"Resident Evil Revelations 2");
+                        discord.UpdateDetails($"Raid Mode: In Lobby");
+                        discord.UpdateState($"{raid_character} (Lv. {raid_character_level})");
+                    }
+                    else
+                    {
+                        discord.UpdateLargeAsset($"logo", $"Resident Evil Revelations 2");
+                        discord.UpdateDetails($"Raid Mode: Mission {chapter}-{mission}");
+                        discord.UpdateState($"{raid_character} (Lv. {raid_character_level})");
+                    }
                 }
                 else
                 {
