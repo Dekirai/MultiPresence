df163b59100db707222a49b388f427648fd3c7f2
Dekirai <keivo1997@gmail.com>
2024-03-05 12:38:47 +0100

    Update Stages.cs

--- MultiPresence/Models/REV2/Stages.cs
+++ MultiPresence/Models/REV2/Stages.cs
@@ -13,59 +13,23 @@
                     getstage.Add("In Lobby");
                     break;
                 case 0x08FC:
-                    getstage.Add("Raid Mode");
-                    break;
                 case 0x0410:
-                    getstage.Add("Raid Mode");
-                    break;
                 case 0x0837:
-                    getstage.Add("Raid Mode");
-                    break;
                 case 0x0835:
-                    getstage.Add("Raid Mode");
-                    break;
                 case 0x0514:
-                    getstage.Add("Raid Mode");
-                    break;
                 case 0x0838:
-                    getstage.Add("Raid Mode");
-                    break;
                 case 0x08FE:
-                    getstage.Add("Raid Mode");
-                    break;
                 case 0x09C9:
-                    getstage.Add("Raid Mode");
-                    break;
                 case 0x03E9:
-                    getstage.Add("Raid Mode");
-                    break;
                 case 0x044E:
-                    getstage.Add("Raid Mode");
-                    break;
                 case 0x04B5:
-                    getstage.Add("Raid Mode");
-                    break;
                 case 0x041A:
-                    getstage.Add("Raid Mode");
-                    break;
                 case 0x05DF:
-                    getstage.Add("Raid Mode");
-                    break;
                 case 0x08FF:
-                    getstage.Add("Raid Mode");
-                    break;
                 case 0x04B1:
-                    getstage.Add("Raid Mode");
-                    break;
                 case 0x0900:
-                    getstage.Add("Raid Mode");
-                    break;
                 case 0x03E8:
-                    getstage.Add("Raid Mode");
-                    break;
                 case 0x0840:
-                    getstage.Add("Raid Mode");
-                    break;
                 case 0x0517:
                     getstage.Add("Raid Mode");
                     break;
