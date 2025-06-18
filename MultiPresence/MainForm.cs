fcb757ce73e65ffeba5afb119b9880eedd367efd
Dekirai <keivo1997@gmail.com>
2025-06-18 12:59:08 +0200

    fuck

--- MultiPresence/MainForm.cs
+++ MultiPresence/MainForm.cs
@@ -20,7 +20,7 @@ namespace MultiPresence
         public static bool isBlacklistLoaded = false;
 
         private static readonly string githubRepo = "Dekirai/MultiPresence";
-        private static readonly string currentVersion = "17.06.2025";
+        private static readonly string currentVersion = "18.06.2025";
         private static readonly string tempUpdaterPath = Path.Combine(Path.GetTempPath(), "Updater.exe");
 
         public MainForm()
