a71ab5d6d46fb8749563f6599d804d364e34bed9
Dekirai <keivo1997@gmail.com>
2026-07-14 22:19:22 +0200

    Start refactored runtime

--- MultiPresence/Program.cs
+++ MultiPresence/Program.cs
@@ -2,17 +2,14 @@ namespace MultiPresence
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            MainForm frm = new MainForm();
+
+            using MainForm frm = new();
             frm.Visible = false;
+            frm.EnableRefactoredRuntime();
             Application.Run();
         }
     }
